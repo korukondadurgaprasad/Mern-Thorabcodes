496ed77b207a5eb0638303e876b76eacd1a8a931
korukondadurgaprasad <prasad7288033@gmail.com>
2025-01-27 12:09:55 +0530

    new file:   adc.txt 	new file:   api_fundamentals/inde.js 	new file:   index.js 	new file:   products.txt

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs =require('fs');
+const os =require('os');
+
+// sync way of reading
+// let data=fs.readFileSync('./adc.txt','utf-8');
+// console.log(data)
+
+
+
+
+
+
+//async way of reading 
+
+// console.log('Line 1')
+// fs.readFile('./adc.txt','utf-8',(err,data)=>{
+//     console.log(err);
+//     console.log(data);
+
+// })
+
+// console.log("Some random Code")
+
+
+
+
+
+
+//Write file
+// fs.writeFileSync('./products.txt','Apple');
+
+// fs.writeFile('./products.txt','Mongo',(err)=>{
+//     console.log(err);
+// }) 
+// removing the past a data and adding new data appale is replaced with mango
+
+// fs.appendFile('./products.txt','\nGrapes',(err)=>{
+//     console.log(err);
+// })
+
+
+
+
+
+console.log(os.platform())
+console.log(os.hostname())
+console.log(os.type())
