cdf6916a2dbc651310f7da9fc42978936e4e2862
korukondadurgaprasad <prasad7288033@gmail.com>
2025-02-16 15:57:39 +0530

    Changes to be committed: 	new file:   api_fundamentals/dummy.html 	modified:   api_fundamentals/index.js 	modified:   api_fundamentals/products.json 	new file:   db-work/index.js 	new file:   db-work/package-lock.json 	new file:   db-work/package.json
    Untracked files:
    	db-work/node_modules/

--- api_fundamentals/index.js
+++ api_fundamentals/index.js
@@ -28,12 +28,11 @@
 // const fs = require('fs');
 // const url = require('url');
 
-
 // http.createServer((req, res) => {
 
-//     let parsedUrl = url.parse(req.url,true);
+//     let parsedUrl = url.parse(req.url,true);  // ParseUrl= this help to divide the url properly and 
 //     console.log(parsedUrl)
-//         if (req.url === '/products' && req.method === 'GET') {
+//     if (parsedUrl.pathname === '/products' && req.method === 'GET') {
 //         fs.readFile("./products.json", 'utf-8', (err, data) => {
 //             if (err == null) {
 //                 res.end(data); // Sending the contents of products.json
@@ -42,9 +41,12 @@
 //             }
 //         });
 //     }
-    
+
 
 // }).listen(8000)
+// http://127.0.0.1:8000/products?id=2&name=prasad
+
+
 
 
 
@@ -80,7 +82,7 @@
 //             res.end(JSON.stringify({"message":'product not found'}))
 //         }
 //     }
-    
+
 
 // }).listen(8000)
 
@@ -92,60 +94,240 @@
 
 // POST/Products
 
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url'); 
+
+
+// http.createServer((req, res) => {
+
+//     let parsedUrl = url.parse(req.url,true);
+//     console.log(parsedUrl.query.id)
+
+// // readind the file asa string 
+//     let products=fs.readFileSync('./products.json',"utf-8")
+
+// //fetch all products
+
+//     if(parsedUrl.pathname=="/products" && req.method=="GET" && parsedUrl.query.id==undefined){
+//         res.end(products)
+//     }
+//     //fetch all products based on id
+//     else if(parsedUrl.pathname=="/products" && req.method=="GET" && parsedUrl.query.id!=undefined)
+//     {
+//         let productsArray=JSON.parse(products);
+
+//         let a= productsArray.find((product)=>{//i need to post the data to the array based on id so i have converted to string to
+//             return product.id==parsedUrl.query.id;
+
+//         })
+//         if (a!=undefined){
+//             res.end(JSON.stringify(a))
+//         }
+//         else{
+//             res.end(JSON.stringify({"message":'product not found'}))
+//         }
+//     }
+//      // create new product
+//     else if (req.method=='POST' && parsedUrl.pathname=="/products")
+//     {
+
+//         let product='';
+//         //this event called for every chuk recweived
+//         req.on("data",(chunk)=>{
+//             product+=chunk
+
+//         })
+//         // this event is called at the end of the stream and converts bytes to readble string 
+//         req.on("end",()=>{
+//             let productsArray=JSON.parse(products);
+//             let newproduct=JSON.parse(product);
+
+//             productsArray.push(newproduct);
+
+//             fs.writeFile("./products.json",JSON.stringify(productsArray),(err)=>{
+//                 if(err==null){
+//                     res.end(JSON.stringify({"message":'product added'}))
+
+//                 }
+
+//             })
+//         })
+//         res.end("post working preperly")
+//     }
+
+
+// }).listen(8000)
+
+
+
+
+// Delete/products 
+// 1. get the id of the product to be deleted
+// 2. find the product in the array and delete it
+// 3. write the updated array to the file
+// 4. send the response back to the client
+// 5. if the product is not found send a message to the client
+// 6. if the product is found send a message to the client that the product has been
+// deleted
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url'); 
+
+
+// http.createServer((req, res) => {
+
+//     let parsedUrl = url.parse(req.url,true);
+//     console.log(parsedUrl.query.id)
+
+// // readind the file asa string 
+//     let products=fs.readFileSync('./products.json',"utf-8")
+
+// //fetch all products
+
+//     if(parsedUrl.pathname=="/products" && req.method=="GET" && parsedUrl.query.id==undefined){
+//         res.end(products)
+//     }
+//     //fetch all products based on id
+//     else if(parsedUrl.pathname=="/products" && req.method=="GET" && parsedUrl.query.id!=undefined)
+//     {
+//         let productsArray=JSON.parse(products);
+
+//         let a= productsArray.find((product)=>{//i need to post the data to the array based on id so i have converted to string to
+//             return product.id==parsedUrl.query.id;
+
+//         })
+//         if (a!=undefined){
+//             res.end(JSON.stringify(a))
+//         }
+//         else{
+//             res.end(JSON.stringify({"message":'product not found'}))
+//         }
+//     }
+//      // create new product
+//     else if (req.method=='POST' && parsedUrl.pathname=="/products")
+//     {
+
+//         let product='';
+//         //this event called for every chuk recweived
+//         req.on("data",(chunk)=>{
+//             product+=chunk
+
+//         })
+//         // this event is called at the end of the stream and converts bytes to readble string 
+//         req.on("end",()=>{
+//             let productsArray=JSON.parse(products);
+//             let newproduct=JSON.parse(product);
+
+//             productsArray.push(newproduct);
+
+//             fs.writeFile("./products.json",JSON.stringify(productsArray),(err)=>{
+//                 if(err==null){
+//                     res.end(JSON.stringify({"message":'product added'}))
+
+//                 }
+
+//             })
+//         })
+//         res.end("post working preperly")
+//     }
+//     else if(req.method='DELETE' && parsedUrl.pathname=='/products'){
+//         let id=parsedUrl.query.id
+
+//         let productsArray =JSON.parse(products);
+//         let index =productsArray.findIndex((product)=>{
+//             return product.id==parsedUrl.query.id
+//         })
+
+//         productsArray.splice(index,1)
+//         fs.writeFile("./products.json",JSON.stringify(productsArray),(err)=>{
+//             if(err==null){
+//                 res.end(JSON.stringify({"message":'product successfully deleted'}))
+
+//             }
+//             else{
+//                 res.end(JSON.stringify({"message":'product not found'}))
+//             }
+
+//         })
+
+
+
+//     }
+
+
+// }).listen(8000)
+
+
+// PUT/products?id=3|data in request body
+
 const http = require('http');
 const fs = require('fs');
-const url = require('url'); 
+const url = require('url');
 
 
 http.createServer((req, res) => {
 
-    let parsedUrl = url.parse(req.url,true);
+    let parsedUrl = url.parse(req.url, true);
     console.log(parsedUrl.query.id)
 
-// readind the file asa string 
-    let products=fs.readFileSync('./products.json',"utf-8")
 
-//fetch all products
 
-    if(parsedUrl.pathname=="/products" && req.method=="GET" && parsedUrl.query.id==undefined){
+
+    // readind the file asa string 
+    let products = fs.readFileSync('./products.json', "utf-8")
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET", "PUT", "PATCH", "DELETE", "OPTIONS");
+
+
+    //fetch all products
+
+    // HANDLING OPTIONS PREFILGHT REQUEST WHICH COMES BEFORE POST PUT AND DELETE AUTOMATICALLY.
+    if (req.method == "OPTIONS") {
+        res.end()
+
+    }
+
+    else if (parsedUrl.pathname == "/products" && req.method == "GET" && parsedUrl.query.id == undefined) {
         res.end(products)
     }
     //fetch all products based on id
-    else if(parsedUrl.pathname=="/products" && req.method=="GET" && parsedUrl.query.id!=undefined)
-    {
-        let productsArray=JSON.parse(products);
+    else if (parsedUrl.pathname == "/products" && req.method == "GET" && parsedUrl.query.id != undefined) {
+        let productsArray = JSON.parse(products);
 
-        let a= productsArray.find((product)=>{//i need to post the data to the array based on id so i have converted to string to
-            return product.id==parsedUrl.query.id;
+        let a = productsArray.find((product) => {//i need to post the data to the array based on id so i have converted to string to
+            return product.id == parsedUrl.query.id;
 
         })
-        if (a!=undefined){
+        if (a != undefined) {
             res.end(JSON.stringify(a))
         }
-        else{
-            res.end(JSON.stringify({"message":'product not found'}))
+        else {
+            res.end(JSON.stringify({ "message": 'product not found' }))
         }
     }
-     // create new product
-    else if (req.method=='POST' && parsedUrl.pathname=="/products")
-    {
+    // create new product
+    else if (req.method == 'POST' && parsedUrl.pathname == "/products") {
 
-        let product='';
+        let product = '';
         //this event called for every chuk recweived
-        req.on("data",(chunk)=>{
-            product+=chunk
+        req.on("data", (chunk) => {
+            product += chunk
 
         })
         // this event is called at the end of the stream and converts bytes to readble string 
-        req.on("end",()=>{
-            let productsArray=JSON.parse(products);
-            let newproduct=JSON.parse(product);
+        req.on("end", () => {
+            let productsArray = JSON.parse(products);
+            let newproduct = JSON.parse(product);
 
             productsArray.push(newproduct);
 
-            fs.writeFile("./products.json",JSON.stringify(productsArray),(err)=>{
-                if(err==null){
-                    res.end(JSON.stringify({"message":'product added'}))
+            fs.writeFile("./products.json", JSON.stringify(productsArray), (err) => {
+                if (err == null) {
+                    res.end(JSON.stringify({ "message": 'product added' }))
 
                 }
 
@@ -153,6 +335,66 @@ http.createServer((req, res) => {
         })
         res.end("post working preperly")
     }
-    
+    else if (req.method == 'DELETE' && parsedUrl.pathname == '/products') {
+        let id = parsedUrl.query.id
+
+        let productsArray = JSON.parse(products);
+        let index = productsArray.findIndex((product) => {
+            return product.id == parsedUrl.query.id
+        })
+
+        productsArray.splice(index, 1)
+        fs.writeFile("./products.json", JSON.stringify(productsArray), (err) => {
+            if (err == null) {
+                res.end(JSON.stringify({ "message": 'product successfully deleted' }))
+
+            }
+            else {
+                res.end(JSON.stringify({ "message": 'product not found' }))
+            }
+
+        })
+    }
+    else if (req.method == 'PUT' && parsedUrl.pathname == "/products") {
+        let id = parsedUrl.query.id
+        let product = "";
+        req.on("data", (chunk) => {
+            product += chunk
+        })
+        req.on("end", () => {
+            let productsArray = JSON.parse(products);
+            let newproduct = JSON.parse(product);
+
+            let index = productsArray.findIndex((product) => {
+                return product.id == parsedUrl.query.id
+            })
+
+            if (index !== -1) {
+                productsArray[index] == newproduct;
+
+                fs.writeFile("./products.json", JSON.stringify(productsArray), (err) => {
+                    if (err == null) {
+                        res.end(JSON.stringify({ "message": 'product' }))
+
+                    }
+                    else {
+                        res.end(JSON.stringify({ "message": 'product not found' }))
+                    }
+
+                })
+
+
+            }
+            else {
+                res.end(JSON.stringify({ "message ": " element with given id is not found" }))
+            }
+
+
+
+        })
+
+    }
+
+
 
 }).listen(8000)
---
+++ db-work/index.js
@@ -0,0 +1,143 @@
+
+// Mongoose is an Object Data Modeling (ODM) library for MongoDB in Node.js. 
+// It provides a structured way to interact with MongoDB databases by defining schemas and models for data.
+
+const { ConnectionCheckOutFailedEvent } = require('mongodb')
+const mongoose = require('mongoose')
+const { type } = require('os')
+// connection to mongo server
+
+mongoose.connect('mongodb://localhost:27017/apidev_demo')
+    .then(() => {
+        console.log("Connection Succesfull")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+
+// In Node.js, a schema defines the structure and properties of data. 
+// It acts as a blueprint for how data should be organized and validated, 
+// ensuring consistency and integrity within an application. 
+//schema
+
+
+const userSchema = mongoose.Schema({
+    // name:String,
+    // age:Number,
+
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        min: 8,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 20,
+        max: 100
+    }
+},{timestamps:true})
+
+
+
+// model  conection between the schema and collection
+const userModel = mongoose.model('users', userSchema);
+// model is a class that we can use to create new documents in the collection.
+// we can use the model to create new documents, read existing documents, update existing documents, and
+// delete existing documents.
+
+
+
+
+
+// inserting data
+
+let user = {
+    name: "thor",
+    age: 28,
+    password:23456,
+    email: "prasad@gmail.com",
+    role: "admin"
+
+}
+
+userModel.create(user)
+    .then((data) => {
+        console.log(data)
+        console.log("Data Inserted")
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+
+
+
+// fetching data
+
+
+
+
+// userModel.find({name:"prasad"})
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+
+// userModel.find().sort({age:1})
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+
+
+// Delete
+
+// userModel.deleteOne({ age: 24 })
+//     .then((info) => {
+//         console.log(info);
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+
+// userModel.deleteMany({ age: 26 })
+//     .then((info) => {
+//         console.log(info);
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+
+
+
+// update
+// userModel.updateOne( { name: "prasa" },{ age: 24 })
+//     .then((info) => {
+//         console.log(info);
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
